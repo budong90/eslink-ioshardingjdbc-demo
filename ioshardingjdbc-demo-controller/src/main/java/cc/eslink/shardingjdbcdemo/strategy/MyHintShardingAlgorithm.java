01ed37f48e186d79e190050cc18c34004ac4c512
zyk <1580648333@qq.com>
2020-01-03 16:16:36 +0800

    真实节点放在配置文件中

--- ioshardingjdbc-demo-controller/src/main/java/cc/eslink/shardingjdbcdemo/strategy/MyHintShardingAlgorithm.java
+++ ioshardingjdbc-demo-controller/src/main/java/cc/eslink/shardingjdbcdemo/strategy/MyHintShardingAlgorithm.java
@@ -1,11 +1,17 @@
 package cc.eslink.shardingjdbcdemo.strategy;
 
+import cc.eslink.util.CollectionUtil;
 import io.shardingsphere.api.algorithm.sharding.ListShardingValue;
 import io.shardingsphere.api.algorithm.sharding.ShardingValue;
 import io.shardingsphere.api.algorithm.sharding.hint.HintShardingAlgorithm;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *@ClassName MyHintShardingAlgorithm
@@ -15,6 +21,26 @@ import java.util.Collection;
  *@Version 1.0
  **/
 public class MyHintShardingAlgorithm implements HintShardingAlgorithm {
+
+//    private static String actualDataNodes;
+
+    private static List<String> actualDataNodes = new ArrayList<>();
+
+    @Value("${actual-data-nodes}")
+    public void setActualDataNodes(String actualDataNodes) {
+        // actual-data-nodes=\${['','_1p01']}
+        Matcher m = Pattern.compile("\\[(.*)\\]\\}$").matcher(actualDataNodes);
+        if (m.find()) {
+            MyHintShardingAlgorithm.actualDataNodes = Stream.of(m.group(1).split(",")).parallel()
+                    .map(s -> s.substring(Math.max(s.indexOf("_") + 1, 1), s.length() - 1).toLowerCase())
+                    .filter(s -> StringUtils.isNotBlank(s))
+                    .sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
+        System.out.println("分片入参-----------------------------actualDataNodes=" + MyHintShardingAlgorithm.actualDataNodes);
+        if (CollectionUtil.isEmpty(MyHintShardingAlgorithm.actualDataNodes)) {
+            System.out.println("真实节点为空");
+        }
+    }
 
     @Override
     public Collection<String> doSharding(Collection<String> availableTargetNames, ShardingValue shardingValue) {
@@ -26,17 +52,16 @@ public class MyHintShardingAlgorithm implements HintShardingAlgorithm {
         Collection<String> values = ((ListShardingValue<String>) shardingValue).getValues();
         if (logicTable.equalsIgnoreCase(shardingValue.getLogicTableName()) && !values.isEmpty()) {
             String tenantId = values.iterator().next().toLowerCase();
-            switch (tenantId) {
-                case "0185":case "0703":case "1p01":
+            Iterator<String> it = MyHintShardingAlgorithm.actualDataNodes.iterator();
+            while (it.hasNext()) {
+                String prefix = it.next();
+                if (StringUtils.isNotBlank(prefix) && tenantId.startsWith(prefix)) {
                     shardingResults.add(logicTable + "_" + tenantId);
                     break;
-                default:
-                    if (tenantId.startsWith("0a")) {
-                        shardingResults.add(logicTable + "_0a");
-                        break;
-                    }
-                    shardingResults.add(logicTable);
-                    break;
+                }
+            }
+            if (shardingResults.isEmpty()) {
+                shardingResults.add(logicTable);
             }
         }
         System.out.println(String.format("分片结果-----------------------------shardingResults=%s-----------------------------", shardingResults));
